50f1462a0cc6f1e9d3c17d70b7c92c9c2ef9c3a1
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2025-06-22 00:04:10 +0300

    в

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/tailwind.css'
+
+// ВАЖНО: импорт Tailwind стилей
+import './styles/tailwind.css' // Здесь должны быть @tailwind base и т.д.
+// (если ты всё же используешь index.css, можно добавить и его сюда)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
