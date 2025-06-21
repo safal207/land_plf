4d220e6dac8f2523d62d20e5ef07fced6c1c7e90
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2025-06-21 23:07:59 +0300

    ок

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ Укажи имя репозитория после слэша
 export default defineConfig({
   base: '/land_plf/',
   plugins: [react()],
