f21c40caf49dcc5f24ada93299a891ac8307602d
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2025-10-22 21:57:26 +0300

    chore: remove duplicate configs and align references

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssConfig from './postcss.config.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: postcssConfig,
+  },
   base: '/land_plf/',
   build: {
     rollupOptions: {
